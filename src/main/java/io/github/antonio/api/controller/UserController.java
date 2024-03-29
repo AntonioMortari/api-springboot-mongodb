06fcb6fd51a1fb135585b05ce7a6a73b2941baf6
Antonio Mortari <113060294+AntonioMortari@users.noreply.github.com>
2024-03-29 15:29:03 -0300

    feat: created user DTO

--- src/main/java/io/github/antonio/api/controller/UserController.java
+++ src/main/java/io/github/antonio/api/controller/UserController.java
@@ -1,6 +1,7 @@
 package io.github.antonio.api.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.github.antonio.api.domain.User;
+import io.github.antonio.api.dto.UserDTO;
 import io.github.antonio.api.service.UserService;
 
 @RestController
@@ -19,10 +21,11 @@ public class UserController {
 	private UserService service;
 	
 	@GetMapping
-	public ResponseEntity<List<User>> index(){
+	public ResponseEntity<List<UserDTO>> index(){
 		List<User> result = service.findAll();
+		List<UserDTO> listDTO = result.stream().map(user -> new UserDTO(user)).collect(Collectors.toList());
 		
-		return ResponseEntity.ok().body(result);
+		return ResponseEntity.ok().body(listDTO);
 	}
 	
 }
